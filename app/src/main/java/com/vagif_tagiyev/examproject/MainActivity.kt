702eb04e8ae2cf6e479bac7bfd1c4c8d2a131eb8
Vaqif <taqiyevv937@gmail.com>
2022-11-22 20:07:21 +0400

    Dizaynı LinearLayoutda yuxarıdan aşağı düzgün bir şəkildə yığdım. Sadəcə, responsive olmasını düzəldə bilmədim. Dərsdə ona baxarıq :D

--- app/src/main/java/com/vagif_tagiyev/examproject/MainActivity.kt
+++ app/src/main/java/com/vagif_tagiyev/examproject/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +16,11 @@ class MainActivity : AppCompatActivity() {
         travel.commit()
 
         Handler(Looper.getMainLooper()).postDelayed({
-            travel = supportFragmentManager.beginTransaction()
-            travel.replace(R.id.fragmentContainerView,PassengerFragment())
-            travel.commit()
+            travel = supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerView,PassengerFragment())
+                addToBackStack(null)
+                commit()
+            }
         }, 3000)
     }
 }
